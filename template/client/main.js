cdecb50020c2ef018217a170375fc9fab6ff63b3
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-14 10:46:30 +0900

    change vue-resource to axios

--- template/client/main.js
+++ template/client/main.js
@@ -2,9 +2,9 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from 'vue-resource'
+import axios from 'axios'
 
-Vue.use(VueResource) // Web requests, Vue plugin
+Vue.prototype.$http = axios // Web requests
 
 new Vue({
   el: '#app',
